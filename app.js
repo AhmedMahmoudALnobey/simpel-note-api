fdb7d2e91fca65f0cca1cbfdbe2cad5cfb39a26f
AhmedMahmoudALnobey <ahmdaln073@gmail.com>
2025-06-17 09:33:17 +0300

    some update in code

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.json());
 app.use('/api/notes', noteRoutes);
 app.listen(3000, () => {
-  console.log(`Server running on port 3000`);
+  console.log("Server running on port 3000");
 });
